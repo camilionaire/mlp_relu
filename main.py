b8f1d39abfb128b357bb55661a2b59bf19364606
Camilo <camilo3@pdx.edu>
2021-09-21 12:59:22 -0700

    added .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import numpy as np
 
 def main():
-	print("Hello World, do I remember how to do this?")
+	# basic print statements and loading of data...
+	print("Starting program...")
+	print("Loading test data...")
+	raw_test = np.loadtxt("csv/mnist_test.csv", delimiter=",", skiprows=1)
+	print("Loading training data...")
+	raw_train = np.loadtxt("csv/mnist_train.csv", delimiter=",", skiprows=1)
 
 
 if __name__ == "__main__":
