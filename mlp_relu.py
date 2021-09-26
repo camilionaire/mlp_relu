b3ffbbedeaef786d9786429811afbc8aa95cf3ae
Camilo <camilo3@pdx.edu>
2021-09-25 22:18:32 -0700

    finish forward prop with relu activation function

--- mlp_relu.py
+++ mlp_relu.py
@@ -13,14 +13,21 @@ class MLPReLU:
 		self.trl = trl
 		self.tstl = tstl
 
-		# I don't know if I actually need this?...
-		# self.hidden1 = first
-		# self.hidden2 = second
-		# self.output = output
+		#I don't know if I actually need this?...
+		self.hidden1size = first
+		self.hidden2size = second
+		self.outputsize = output
+
+		self.hidden1 = []
+		self.hidden2 = []
+		self.output = []
 
 		# so we already add in the bias to the inputs here.
-		self.trin = np.concatenate((np.ones((self.numtr, 1)), trin), axis=1) 
-		self.tstin = np.concatenate((np.ones((self.numtst, 1)), tstin), axis=1)
+		# taking that out and doing it in forward propagation
+		self.trin = trin
+		self.tstin = tstin
+		# self.trin = np.concatenate((np.ones((self.numtr, 1)), trin), axis=1) 
+		# self.tstin = np.concatenate((np.ones((self.numtst, 1)), tstin), axis=1)
 
 		# sets the weights between levels (including that for bias node)
 		self.firstweights = np.random.rand(self.numin + 1, first) / 10 - .05
@@ -32,23 +39,55 @@ class MLPReLU:
 			f"each.\n{self.numtst} examples to test on.\n{first + 1}", \
 				f"and {second + 1} nodes in the middle for {output} outputs.")
 
-	def forward_propagation(self, row, label):
+	def forward_propagation(self, row):
+		# NOTE might want to change all bias nodes to .1 instead.
+		bias_row = np.concatenate(([1], row))
 		# I need some activation function here I think, between the rows.
-		hidden1 = np.concatenate(([1], np.dot(row, self.firstweights)))
-		hidden1 = np.where(hidden1 < 0, 0, hidden1)
-		print("Hidden1: n/", hidden1)
+		self.hidden1 = np.concatenate(([1], np.dot(bias_row, self.firstweights)))
+		self.hidden1 = np.where(self.hidden1 < 0, 0, self.hidden1)
+		print("Hidden1: \n", self.hidden1)
 
-		hidden2 = np.concatenate(([1], np.dot(hidden1, self.secndweights)))
-		hidden2 = np.where(hidden2 < 0, 0, hidden2)
-		print("Hidden2: \n", hidden2)
+		self.hidden2 = np.concatenate(([1], np.dot(self.hidden1, self.secndweights)))
+		self.hidden2 = np.where(self.hidden2 < 0, 0, self.hidden2)
+		print("Hidden2: \n", self.hidden2)
 
-		output = np.dot(hidden2, self.thirdweights)
-		output = np.where(output < 0, 0, output)
-		print("Output: \n", output)
-		print("Label: ", label)
+		self.output = np.dot(self.hidden2, self.thirdweights)
+		self.output = np.where(self.output < 0, 0, self.output)
+		print("Output: \n", self.output)
+		# print("Label: ", label)
+
+		# truth = False
+		# maxi = max(output)
+		# for i in range(0, self.outputsize):
+		# 	if output[i] == maxi:
+		# 		guess = i
+		# 		if i == label:
+		# 			truth = True
+		# return truth
+
+	def check_if_matches(self, label):
+		truth = False
+		maxi = max(self.output)
+		for i in range(0, self.outputsize):
+			if self.output[i] == maxi:
+				guess = i
+				if i == label:
+					truth = True
+		return truth
+
+	def forw_and_back(self):
+		# loads in 1 layer at a time, affects self.hidden1/2/output
+		self.forward_propagation(self.tstin[0])
+		print("label: ", self.tstl[0])
+		if not self.check_if_matches(self.tstl[0]):
+			self.backward_propagation()
+
+	def backward_propagation(self):
+		print("This is back propagation!!! Hello!")
+
 
 	def testing(self):
-		self.forward_propagation(self.tstin[0], self.tstl[0])
+		self.forw_and_back()
 
 
 	def add_one(self, x):
